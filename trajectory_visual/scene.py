1cbf4658f7fbed03630b469c12910031b7c6a849
sid <skninja6@gmail.com>
2021-03-02 10:33:28 -0800

    speedy runs

--- trajectory_visual/scene.py
+++ trajectory_visual/scene.py
@@ -2,6 +2,14 @@ from manim import *
 from parse import *
 from math import atan, pi,radians, cos, sin
 
+class PointAnim(Animation):
+    def __init__(self, point, start_time, rate_func, scene, wmobject, *kwargs):
+        self.point =point
+        self.start_time = start_time
+        self.scene = scene
+        self.mobject = mobject
+        scene.add(point)
+
 class TimedAnimationGroup(AnimationGroup):
     '''
     Timed animations may be defined by setting 'start_time' and 'end_time' or 'run_time' in CONFIG of an animation.
@@ -51,12 +59,11 @@ class VisualizeTrajectory(MovingCameraScene):
         animations = []
         
         line_half = 5
-        self.camera_frame.set_width(50)
-        self.camera_frame.set_height(30)
+        self.camera_frame.set_width(30)
+        self.camera_frame.set_height(20)
         parser = Parser("truncated.txt")
         corner_bot = Square()
         corner_bot.move_to(5*RIGHT + 3*UP)
-        self.add(corner_bot)
         
 
         sq = Square()
@@ -66,6 +73,8 @@ class VisualizeTrajectory(MovingCameraScene):
         self.add(robot)
 
         #should start at first waypt
+
+        t_og = 122.700924
         t = 122.700924
         x = -8.923275006028492
         y = -8.190662038694752
@@ -76,55 +85,53 @@ class VisualizeTrajectory(MovingCameraScene):
         
         self.play(Rotate(robot, -pi/2 + radians(start_angle)))
         # print(start_angle)
-        # robot.move_to([x, y, start_angle])
+        robot.move_to([x, y, start_angle])
         for c in parser.commands:
             if(type(c) is WaypointCommand):
                 self.add(Dot(color=RED, point=[c.x, c.y, 0]))
             else:
                 continue
         for c in parser.commands:
-            if (type(c) is WaypointCommand):
-                continue
-            elif(type(c) is PositionCommand):
+            if(type(c) is PositionCommand):
                 dt = c.t - t
                 dx = c.x - x
                 dy = c.y - y
-                print(start_angle)
                 dtheta = c.theta - start_angle
-                print(dtheta)
 
+            
                 path = Line([x,y,0], [c.x, c.y, 0])
+
+                animations.append(Rotate(robot,angle=radians(-dtheta), start_time=c.t, run_time = dt, rate_func=rate_functions.linear))
+                animations.append(MoveAlongPath(robot, path, start_time=c.t,run_time=dt, rate_func=rate_functions.linear))
                 
                 t = c.t
                 x = c.x
                 y = c.y
 
-                animations.append(Rotate(robot,angle=dtheta, start_time=t, run_time = dt))
-                animations.append(MoveAlongPath(robot, path, start_time=t, run_time=dt, rate_func=rate_functions.linear))
+                
                 # self.play(Rotate(robot,angle=radians(-dtheta) , run_time = dt),MoveAlongPath(robot, path, run_time=dt, rate_func=rate_functions.linear))
                 start_angle = c.theta
                 # print(start_angle)
             elif(type(c) is WaypointHitCommand):
-                dot = Dot(color=GREEN, point=[c.x, c.y, 0])
-                # animations.append(FadeIn(dot))
-                self.add(dot)
+                print("Waypoint Hit")
+                # dot = Dot(color=GREEN, point=[c.x, c.y, 0])
+                animations.append(ShowCreation(Dot(color=GREEN, point=[c.x, c.y, 0]),start_time=t, rate_func=rate_functions.linear))
             elif(type(c) is TargetCommand):
                 dot = Dot(color=BLUE, point=[c.x, c.y, 0])
-                # animations.append(FadeIn(dot))
-                self.add(dot)
+                animations.append(ShowCreation(dot, start_time=t))
             elif(type(c) is VelocityCommand):
-                dt = c.t - t
+                dt_this = c.t-t
 
                 left_arrow = Arrow(4*RIGHT+4*UP, 4*RIGHT+4*UP+[0, c.v_l, 0])
                 right_arrow = Arrow(6*RIGHT+4*UP,6*RIGHT+4*UP+[0, c.v_r, 0])
 
-                self.add(curr_vel_left)
-                self.add(curr_vel_right)
-                animations.append(Transform(curr_vel_left, left_arrow, start_time=t, run_time=dt))
-                animations.append(Transform(curr_vel_right, right_arrow, start_time=t,run_time = dt))
+                animations.append(ReplacementTransform(curr_vel_left, left_arrow, start_time=c.t, run_time=dt_this))
+                animations.append(ReplacementTransform(curr_vel_right, right_arrow, start_time=c.t,run_time = dt_this))
+
                 # self.play(Transform(curr_vel_left, left_arrow, run_time=dt), Transform(curr_vel_right, right_arrow, run_time = dt))
                 # self.play(FadeOut(curr_vel_left), FadeOut(curr_vel_right))
                 curr_vel_left = left_arrow
                 curr_vel_right = right_arrow
-            self.play(TimedAnimationGroup(*animations))
-            
+        print(animations)
+        self.play(TimedAnimationGroup(*animations))
+        
