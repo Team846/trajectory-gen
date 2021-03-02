ba529364cf99e26f5f7337f559d1bec5171e3b72
Andrew (Andy) Min <me@andrewmin.info>
2021-03-01 22:40:18 -0800

    Parser

--- trajectory_visual/parse.py
+++ trajectory_visual/parse.py
@@ -2,6 +2,45 @@ from typing import List, Any
 from manim import *
 import numpy as np
 
+
+class MarkerCommand:
+    def __init__(self, line: List[Any]):
+        if line[0] != "MARKER":
+            raise ValueError()
+
+        self.x = float(line[1])
+        self.y = float(line[2])
+
+
+class WaypointCommand:
+    def __init__(self, line: List[Any]):
+        if line[0] != "WAYPOINT":
+            raise ValueError()
+
+        self.x = float(line[1])
+        self.y = float(line[2])
+
+
+class TargetCommand:
+    def __init__(self, line: List[Any]):
+        if line[0] != "TARGET":
+            raise ValueError()
+
+        self.t = float(line[1])
+        self.x = float(line[2])
+        self.y = float(line[3])
+
+
+class WaypointHitCommand:
+    def __init__(self, line: List[Any]):
+        if line[0] != "WAYPOINT_HIT":
+            raise ValueError()
+
+        self.t = float(line[1])
+        self.x = float(line[2])
+        self.y = float(line[3])
+
+
 class PositionCommand:
     def __init__(self, line: List[Any]):
         if line[0] != "POSITION":
@@ -12,8 +51,15 @@ class PositionCommand:
         self.y = float(line[3])
         self.theta = float(line[4])
 
-    def __str__(self):
-        return f"POSITION {self.t} {self.x} {self.y} {self.theta}"
+
+class VelocityCommand:
+    def __init__(self, line: List[Any]):
+        if line[0] != "VELOCITY":
+            raise ValueError()
+
+        self.t = float(line[1])
+        self.v_l = float(line[2])
+        self.v_r = float(line[3])
 
 
 class Parser:
@@ -21,7 +67,12 @@ class Parser:
         self.commands = []
 
         command_types = {
+            "MARKER": MarkerCommand,
+            "WAYPOINT": WaypointCommand,
+            "TARGET": TargetCommand,
+            "WAYPOINT_HIT": WaypointHitCommand,
             "POSITION": PositionCommand,
+            "VELOCITY": VelocityCommand,
         }
 
         with open(file_name, "r") as file:
