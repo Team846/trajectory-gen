acaf1a355223be619c36232505d56c6461e3557c
sid <skninja6@gmail.com>
2021-03-03 21:43:14 -0800

    wypts anim

--- trajectory_visual/follow.py
+++ trajectory_visual/follow.py
@@ -51,28 +51,30 @@ class VisualizeTrajectory(MovingCameraScene):
         # # print(start_angle)
         # robot.shift([x, y, 0])
         for c in parser.commands:
-            if(type(c) is PositionCommand):
-                dt = c.t - t
-                dx = c.x - x
-                dy = c.y - y
-                dtheta = c.theta - start_angle
+            # if(type(c) is PositionCommand):
+            #     dt = c.t - t
+            #     dx = c.x - x
+            #     dy = c.y - y
+            #     dtheta = c.theta - start_angle
 
             
-                path = Line([x,y,0], [c.x, c.y, 0])
+            #     path = Line([x,y,0], [c.x, c.y, 0])
 
-                robot_movements.append(AnimationGroup(Rotate(robot,angle=radians(-dtheta), run_time = dt, rate_func=rate_functions.linear), MoveAlongPath(robot, path, run_time=dt, rate_func=rate_functions.linear), self.camera.frame.animate.move_to([c.x, c.y, 0])))                
-                t = c.t
-                x = c.x
-                y = c.y
+            #     robot_movements.append(AnimationGroup(Rotate(robot,angle=radians(-dtheta), run_time = dt, rate_func=rate_functions.linear), MoveAlongPath(robot, path, run_time=dt, rate_func=rate_functions.linear), self.camera.frame.animate.move_to([c.x, c.y, 0])))                
+            #     t = c.t
+            #     x = c.x
+            #     y = c.y
 
-                start_angle = c.theta
-            # if(type(c) is WaypointCommand):
-            #     self.play(FadeIn(Dot([c.x, c.y, 0]), start_time=c.t, run_time=dt))
+            #     start_angle = c.theta
+            if(type(c) is WaypointCommand):
+                dot = Dot([c.x, c.y, 0])
+                self.add(dot)
+                self.play(FadeIn(dot, run_time = c.t-t))
             # elif(type(c) is WaypointHitCommand):
             #     dot = Dot(color=RED, point=[c.x, c.y, 0])
             #     dot_2 = Dot(color=GREEN, point=[c.x, c.y, 0])
             #     target_vec = Arrow([c.x, c.y, 0], [c.new_target_x, c.new_target_y, 0], color=BLUE)
             #     robot_movements.append(Succession(ReplacementTransform(dot,VGroup(dot_2, target_vec), run_time = c.t - t), FadeOut(target_vec), lag_time=0.1))
-        print(robot_movements)
-        self.play(AnimationGroup(Succession(*robot_movements)))
+        # print(robot_movements)
+        # self.play(AnimationGroup(Succession(*robot_movements)))
         
